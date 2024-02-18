345517cfdadf7921bb1191fa72d79c65e0d2a5a5
anonimus <148051645+cosminnionutt@users.noreply.github.com>
2024-02-18 18:18:53 +0200

    Add files via upload

---
+++ enhancer/customize.sh
@@ -0,0 +1,153 @@
+#!/sbin/sh
+#
+# KnoxPatch Enhancer
+# Copyright (C) 2023 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+SKIPUNZIP=1
+
+enforce_sem() {
+  ONEUI_VERSION=$(grep_prop 'ro.build.version.oneui')
+
+  if [ -z "$ONEUI_VERSION" ]; then
+    SEP_VERSION=$(grep_prop 'ro.build.version.sep')
+
+    if [ -z "$SEP_VERSION" ]; then
+      abort "E: This module only supports One UI devices"
+    fi
+
+    # The OneUI version is determined by the SEP version
+    # (SEP 14.0 == OneUI 5.0 | [SEP - 9.0 = OneUI] )
+    ONEUI_VERSION=$((SEP_VERSION - 90000))
+  fi
+
+  if [ ! "$ONEUI_VERSION" -ge "10000" ]; then
+    abort "E: This module only supports One UI 1.x and above"
+  fi
+
+  SEP_LITE=false
+  [ -f "/system/etc/floating_feature.xml" ] && FEATURE_PATH="/system/etc" || FEATURE_PATH="/vendor/etc"
+  grep -q 'sep_lite' "$FEATURE_PATH/floating_feature.xml" && SEP_LITE=true
+  [ -f "/system/etc/permissions/com.samsung.feature.samsung_experience_mobile_lite.xml" ] && SEP_LITE=true
+  if $SEP_LITE; then
+    LABEL="One UI Core"
+  else
+    LABEL="One UI"
+  fi
+
+  MAJOR=$((ONEUI_VERSION / 10000))
+  MINOR=$((ONEUI_VERSION % 10000 / 100))
+  PATCH=$((ONEUI_VERSION % 100))
+
+  if [ $PATCH != "0" ]; then
+    ui_print "I: $LABEL version: $MAJOR.$MINOR.$PATCH"
+  else
+    ui_print "I: $LABEL version: $MAJOR.$MINOR"
+  fi
+}
+
+VERSION=$(grep_prop version "${TMPDIR}/module.prop")
+ui_print "I: KnoxPatch Enhancer: ${VERSION}"
+
+# Extract verify.sh
+ui_print "- Extracting verify.sh"
+unzip -o "$ZIPFILE" 'verify.sh' -d "$TMPDIR" >&2
+if [ ! -f "$TMPDIR/verify.sh" ]; then
+  ui_print "*********************************************************"
+  ui_print "! Unable to extract verify.sh!"
+  ui_print "! This zip may be corrupted, please try downloading again"
+  abort    "*********************************************************"
+fi
+. "$TMPDIR/verify.sh"
+
+extract "$ZIPFILE" 'customize.sh' "$TMPDIR"
+extract "$ZIPFILE" 'verify.sh' "$TMPDIR"
+
+if $BOOTMODE; then
+  ui_print "- Installing from Magisk / KernelSU app"
+
+  enforce_sem
+
+  if pm list packages | grep -q knoxpatch; then
+    KP_VERSION=$(dumpsys package io.mesalabs.knoxpatch | grep versionName | sed 's/versionName=//g' | sed 's/ //g')
+    ui_print "I: KnoxPatch installed: v$KP_VERSION"
+  else
+    ui_print "W: KnoxPatch not installed!"
+  fi
+
+  ui_print "I: Extracting module files..."
+  extract "$ZIPFILE" 'module.prop' "$MODPATH"
+  extract "$ZIPFILE" 'system.prop' "$MODPATH"
+  mkdir -p "$MODPATH/system/etc/permissions"
+  extract "$ZIPFILE" 'system/etc/permissions/knoxpatch_enhancer.xml' "$MODPATH/system/etc/permissions" true
+
+  ui_print "I: Applying WSM fix..."
+  mkdir -p "$MODPATH/system/lib"
+  touch "$MODPATH/system/lib/libhal.wsm.samsung.so"
+  if $IS64BIT; then
+    mkdir -p "$MODPATH/system/lib64"
+    touch "$MODPATH/system/lib64/libhal.wsm.samsung.so"
+  fi
+
+  set_perm_recursive "$MODPATH" 0 0 0755 0644
+else
+  ui_print "- Installing from recovery"
+
+  enforce_sem
+
+  mount -o remount,rw /system || abort "E: Could not mount system as rw"
+
+  if [ -f "/system/bin/vold.bak" ]; then
+    ui_print "I: Restoring backup files..."
+    mv -f "/system/bin/vold.bak" "/system/bin/vold"
+    [ -f "/system/lib/libepm.so.bak" ] && mv -f "/system/lib/libepm.so.bak" "/system/lib/libepm.so"
+    [ -f "/system/lib64/libepm.so.bak" ] && mv -f "/system/lib64/libepm.so.bak" "/system/lib64/libepm.so"
+  elif [ "$API" == "29" ] || [ "$API" == "30" ]; then
+    if grep -q 'fileencryption' /vendor/etc/fstab.*; then
+      if grep -q 'Knox protection required' /system/bin/vold; then
+        ui_print "I: Applying Secure Folder fix..."
+        cp --preserve=all "/system/bin/vold" "/system/bin/vold.bak"
+        PATCHED=false
+        $PATCHED || hex_patch "/system/bin/vold" 00e4006fea861a11 00e4006feabe0451 && PATCHED=true
+        $PATCHED || hex_patch "/system/bin/vold" 08fa805200e4006f 0800805200e4006f && PATCHED=true
+        $PATCHED || hex_patch "/system/bin/vold" 08fa80520800ae72 080080520800ae72 && PATCHED=true
+        $PATCHED || hex_patch "/system/bin/vold" 09fa80520900ae72 090080520900ae72 && PATCHED=true
+        $PATCHED || abort "E: Failed to apply patch"
+        set_perm "/system/bin/vold" 0 2000 0755 "u:object_r:vold_exec:s0"
+      else
+        ui_print "W: No patches required for this device"
+      fi
+    elif [ "$API" == "29" ] && [ "$ARCH" == "arm64" ]; then
+      if grep -q 'Device supports FBE!' /system/lib/libepm.so; then
+        ui_print "I: Applying Secure Folder fix..."
+        mv "/system/bin/vold" "/system/bin/vold.bak"
+        mv "/system/lib/libepm.so" "/system/lib/libepm.so.bak"
+        mv "/system/lib64/libepm.so" "/system/lib64/libepm.so.bak"
+        extract "$ZIPFILE" 'system/bin/vold' "/system/bin" true
+        extract "$ZIPFILE" 'system/lib/libepm.so' "/system/lib" true
+        extract "$ZIPFILE" 'system/lib64/libepm.so' "/system/lib64" true
+        set_perm "/system/bin/vold" 0 2000 0755 "u:object_r:vold_exec:s0"
+        set_perm "/system/lib/libepm.so" 0 0 0644
+        set_perm "/system/lib64/libepm.so" 0 0 0644
+      else
+        ui_print "W: No patches required for this device"
+      fi
+    fi
+  else
+    ui_print "W: Nothing to do here :/"
+  fi
+
+  mount -o remount,ro /system
+fi
---
+++ enhancer/verify.sh
@@ -0,0 +1,105 @@
+TMPDIR_FOR_VERIFY="$TMPDIR/.vunzip"
+mkdir "$TMPDIR_FOR_VERIFY"
+
+abort_verify() {
+  ui_print "*********************************************************"
+  ui_print "! $1"
+  ui_print "! This zip may be corrupted, please try downloading again"
+  abort    "*********************************************************"
+}
+
+# extract <zip> <file> <target dir> <junk paths>
+extract() {
+  zip=$1
+  file=$2
+  dir=$3
+  junk_paths=$4
+  [ -z "$junk_paths" ] && junk_paths=false
+  opts="-o"
+  [ $junk_paths = true ] && opts="-oj"
+
+  file_path=""
+  hash_path=""
+  if [ $junk_paths = true ]; then
+    file_path="$dir/$(basename "$file")"
+    hash_path="$TMPDIR_FOR_VERIFY/$(basename "$file").sha256"
+  else
+    file_path="$dir/$file"
+    hash_path="$TMPDIR_FOR_VERIFY/$file.sha256"
+  fi
+
+  unzip $opts "$zip" "$file" -d "$dir" >&2
+  [ -f "$file_path" ] || abort_verify "$file not exists"
+
+  unzip $opts "$zip" "$file.sha256" -d "$TMPDIR_FOR_VERIFY" >&2
+  [ -f "$hash_path" ] || abort_verify "$file.sha256 not exists"
+
+  (echo "$(cat "$hash_path")  $file_path" | sha256sum -c -s -) || abort_verify "Failed to verify $file"
+  ui_print "- Verified $file" >&1
+}
+
+# hex_check <file> <hex>
+hex_check() {
+  local xxd
+  if xxd --help >/dev/null 2>&1; then
+    xxd=xxd
+  elif /system/bin/xxd --help >/dev/null 2>&1; then
+    xxd="/system/bin/xxd"
+  elif /system/bin/toybox xxd --help >/dev/null 2>&1; then
+    xxd="/system/bin/toybox xxd"
+  else
+    echo " CANT LOAD XXD bin" && return
+  fi
+  if ! exist "$1"; then echo "CANT FIND: $1" && return 1; fi
+  if $( ( $xxd -p "$1" | tr -d \\n | tr -d " " | grep -q "$2" ) 2>/dev/null ); then true; else false; fi
+}
+
+# hex_patch <file> <old_hex> <new_hex>
+hex_patch() {
+  local xxd
+  if xxd --help >/dev/null 2>&1; then
+    xxd=xxd
+  elif /system/bin/xxd --help >/dev/null 2>&1; then
+    xxd="/system/bin/xxd"
+  elif /system/bin/toybox xxd --help >/dev/null 2>&1; then
+    xxd="/system/bin/toybox xxd"
+  elif [ ! -e "/data/adb/magisk/magiskboot" ]; then
+    echo "CANT LOAD: xxd or magiskboot" && return 1
+  fi
+  if ! exist "$1"; then echo "CANT FIND: $1" && return 1; fi
+  testrw "$(dirname "$1")" || return 1
+  if $( ( $xxd -p "$1" | tr -d \\n | tr -d " " | sed "s/$2/$3/" | $xxd -r -p > "$1.tmp" ) 2>/dev/null ); then
+    if hex_check "$1.tmp" "$3"; then
+      move "$1.tmp" "$1"
+      true
+    elif [ -e "/data/adb/magisk/magiskboot" ]; then
+      if $(/data/adb/magisk/magiskboot hexpatch "$1" "$2" "$3"); then
+        if hex_check "$1" "$3"; then true; else false; fi
+      else
+        false
+      fi
+    else
+      false
+    fi
+  elif [ -e "/data/adb/magisk/magiskboot" ]; then
+    if $(/data/adb/magisk/magiskboot hexpatch "$1" "$2" "$3"); then
+      if hex_check "$1" "$3"; then true; else false; fi
+    else
+      false
+    fi
+  else
+    false
+  fi
+}
+
+file="META-INF/com/google/android/update-binary"
+file_path="$TMPDIR_FOR_VERIFY/$file"
+hash_path="$file_path.sha256"
+unzip -o "$ZIPFILE" "META-INF/com/google/android/*" -d "$TMPDIR_FOR_VERIFY" >&2
+[ -f "$file_path" ] || abort_verify "$file not exists"
+if [ -f "$hash_path" ]; then
+  (echo "$(cat "$hash_path")  $file_path" | sha256sum -c -s -) || abort_verify "Failed to verify $file"
+  ui_print "- Verified $file" >&1
+else
+  ui_print "- Download from Magisk app"
+fi
