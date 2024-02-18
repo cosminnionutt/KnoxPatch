fe28d06e356e6a6e04dbc1e02e16307e99a3f776
anonimus <148051645+cosminnionutt@users.noreply.github.com>
2024-02-18 18:20:44 +0200

    Add files via upload

---
+++ stub/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "io.mesalabs.stub"
+    compileSdk = 34
+    buildToolsVersion = "34.0.0"
+
+    defaultConfig {
+        minSdk = 28
+    }
+
+    buildFeatures {
+        buildConfig = false
+    }
+}
+
+dependencies {
+    // HiddenApiRefinePlugin: https://github.com/RikkaApps/HiddenApiRefinePlugin
+    annotationProcessor("dev.rikka.tools.refine:annotation-processor:4.4.0")
+    compileOnly("dev.rikka.tools.refine:annotation:4.4.0")
+    // AndroidX: https://developer.android.com/jetpack/androidx/versions
+    implementation("androidx.annotation:annotation:1.7.1")
+}
